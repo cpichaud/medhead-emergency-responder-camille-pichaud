1d3c61d4138c190bd57121c75dd2d9f168e0f8ff
cpichaud <camillepichaud44@gmail.com>
2026-03-04 13:54:32 +0100

    Initial: structure du projet générée avec Spring Boot

---
+++ emergency-responder/src/main/java/com/medhead/emergency_responder/EmergencyResponderApplication.java
@@ -0,0 +1,13 @@
+package com.medhead.emergency_responder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyResponderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyResponderApplication.class, args);
+	}
+
+}
---
+++ emergency-responder/src/test/java/com/medhead/emergency_responder/EmergencyResponderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhead.emergency_responder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyResponderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
