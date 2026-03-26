0eae27ed30d2ffb21ea6a11acd37233a8847faad
cpichaud <camillepichaud44@gmail.com>
2026-03-26 10:13:15 +0100

    Init: initialisation du projet Spring Boot et configuration de la base H2

---
+++ emergency-responder/src/main/resources/import.sql
@@ -0,0 +1,22 @@
+INSERT INTO hospital (name, available_beds, latitude, longitude) VALUES ('Royal London Hospital', 15, 51.5180, -0.0594);
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (1, 'Médecine d''urgence');
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (1, 'Chirurgie générale');
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (1, 'Traumatologie et chirurgie orthopédique');
+
+INSERT INTO hospital (name, available_beds, latitude, longitude) VALUES ('Hammersmith Hospital', 3, 51.5165, -0.2396);
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (2, 'Cardiologie');
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (2, 'Chirurgie cardiothoracique');
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (2, 'Médecine respiratoire');
+
+INSERT INTO hospital (name, available_beds, latitude, longitude) VALUES ('Royal Free Hospital', 0, 51.5531, -0.1658);
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (3, 'Cardiologie');
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (3, 'Neurologie');
+
+INSERT INTO hospital (name, available_beds, latitude, longitude) VALUES ('King''s College Dental', 8, 51.4674, -0.0900);
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (4, 'Chirurgie buccale et maxillo-faciale');
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (4, 'Orthodontie');
+
+INSERT INTO hospital (name, available_beds, latitude, longitude) VALUES ('Queen''s Hospital', 12, 51.5683, 0.1795);
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (5, 'Immunologie');
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (5, 'Microbiologie médicale');
+INSERT INTO hospital_specialisms (hospital_id, specialism_name) VALUES (5, 'Virologie');
